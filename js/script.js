6ff0630ad9f9b1ffc6032d691cf8682c0f5eb359
Andrii <smus.andriy@gmail.com>
2024-12-28 22:35:45 +0200

    delete mistake

--- js/script.js
+++ js/script.js
@@ -13,11 +13,10 @@ burger.addEventListener('click', () => {
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false,
-//   },
-autoplay: false,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
