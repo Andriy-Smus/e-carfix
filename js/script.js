55f33097e61ea60bf2a69ecb349b338959115e85
Andrii <smus.andriy@gmail.com>
2024-12-28 22:34:19 +0200

    finished slider

--- js/script.js
+++ js/script.js
@@ -8,3 +8,23 @@ burger.addEventListener('click', () => {
     burgerMenu.classList.toggle('show');
     nav.classList.toggle('show');
 });
+
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+autoplay: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: "true",
+});
