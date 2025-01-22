1fb62457468a140d5939a02f69f24a3b98166a78
Andrii <smus.andriy@gmail.com>
2025-01-22 22:21:38 +0200

    fix finish1

--- send_email.php
+++ send_email.php
@@ -12,8 +12,9 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com'; 
     $mail->SMTPAuth = true;
-    $mail->Username = 'smus.andriy@gmail.com'; 
-    $mail->Password = 'qosy eykx lplk vfix';
+    $mail->Username = 'ecarfix.ua@gmail.com'; 
+    // $mail->Password = 'qosy eykx lplk vfix';
+    $mail->Password = 'd j m o t c d u m z p r n g z s';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
 
@@ -21,7 +22,7 @@ try {
 
     $mail->setFrom('your_email@example.com', 'Сайт E-CARFIX');
 
-    $mail->addAddress('smus.andriy@gmail.com');
+    $mail->addAddress('ecarfix.ua@gmail.com');
 
     $mail->Subject = 'E-CARFIX: Запит на консультацію';
     $mail->Body = "Ім'я: {$_POST['name']}\nТелефон: {$_POST['phone']}\nПовідомлення: {$_POST['text']}";
